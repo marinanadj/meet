8aacbe5f3fc31f76a86549109a8553927fcf42c1
marinanadj <marinanadj7@yahoo.com>
2022-06-15 09:50:40 +0200

    Create Event.js

---
+++ src/Event.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+  state = {
+    showDetails: false,
+  };
+
+  render() {
+    const {
+      summary,
+      location,
+      start,
+      htmlLink,
+      description,
+    } = this.props.event;
+
+    const date = new Date(start.dateTime);
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    };
+    const eventStart = date.toLocaleString("en-US", options);
+
+    const { showDetails } = this.state;
+    return (
+      <div className='event'>
+        <div className='event__Overview'>
+          <h2 className='event__Overview--name'>{summary}</h2>
+          <p className='event__Overview--localDate'>{`${eventStart}`}</p>
+          {location && (
+            <p className='event__Overview--venue'>
+              @{summary} | {location}
+            </p>
+          )}
+          {showDetails && (
+            <button
+              className='details-btn'
+              onClick={() => this.setState({ showDetails: !showDetails })}
+            >
+              hide details
+            </button>
+          )}
+
+          {!showDetails && (
+            <button
+              className='details-btn'
+              onClick={() => this.setState({ showDetails: !showDetails })}
+            >
+              show details
+            </button>
+          )}
+        </div>
+        {showDetails && (
+          <div className='event__Details'>
+            <h3>About event:</h3>
+            <h4>
+              <a href={htmlLink} target='_blank' rel='noopener noreferrer'>
+                See details on Google Calendar
+              </a>
+            </h4>
+            <p
+              className='event__Details--description'
+              dangerouslySetInnerHTML={{ __html: description }}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Event;
