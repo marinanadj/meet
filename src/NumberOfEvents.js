56e330b5c12fad621db99765fabafe5adb8389e2
Marina Nadj <marinanadj7@yahoo.com>
2022-06-22 19:58:23 +0200

    upp

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
-
   state = { 
     numberOfEvents : 1
   }
@@ -12,7 +11,7 @@ class NumberOfEvents extends Component {
     if ((newValue > 33) || (newValue < 1)) {
       this.setState({
         numberOfEvents: newValue,
-        infoText: 'Please choose a number between 1 and 32',
+        infoText: 'Please choose any number between 1 and 32',
       })
     } else {
       this.setState({
@@ -26,7 +25,7 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <>
-      <div className="numberOfEvents text-muted">How many events would you like to see?</div>
+      <div className="numberOfEvents text-muted">How many events would you like to see on this page?</div>
       <input 
             className="events_number__input my-3" 
             type="number" 
--- src/_test.js_/NumberOfEvents.test.js
+++ src/_test.js_/NumberOfEvents.test.js
@@ -1,36 +1,41 @@
-import { shallow } from "enzyme";
-import React from "react";
-import NumberOfEvents from "../NumberOfEvents";
+import React, { Component } from "react";
+import { ErrorAlert } from './App.test';
 
-describe("<NumberOfEvents /> component", () => {
-  let NumberOfEventsWrapper;
-  beforeAll(() => {
-    NumberOfEventsWrapper = shallow(
-      <NumberOfEvents updateNumberOfEvents={() => {}} />
+class NumberOfEvents extends Component {
+  state = { 
+    numberOfEvents : 1
+  }
+
+  changeNumOfEvents = (e) => {
+    let newValue = parseInt(e.target.value);
+    if ((newValue > 33) || (newValue < 1)) {
+      this.setState({
+        numberOfEvents: newValue,
+        infoText: 'Please choose any number between 1 and 32',
+      })
+    } else {
+      this.setState({
+        numberOfEvents: newValue,
+        infoText: ' ',
+      })
+    }
+        this.props.updateEvents(undefined, newValue);
+  }
+
+  render() {
+    return (
+      <>
+      <div className="numberOfEvents text-muted">How many events would you like to see on this page?</div>
+      <input 
+            className="events_number__input my-3" 
+            type="number" 
+            onChange={this.changeNumOfEvents}
+            value={this.state.numberOfEvents}>
+        </input>
+      <ErrorAlert text={this.state.infoText} />
+      </>
     );
-  });
+  }
+}
 
-  test("render textbox element", () => {
-    expect(NumberOfEventsWrapper.find(".numberOfEvents")).toHaveLength(1);
-  });
-
-  test("render text input correctly", () => {
-    const numberOfEvents = NumberOfEventsWrapper.state("numberOfEvents");
-    expect(
-      NumberOfEventsWrapper.find("#numberOfEvents__input").prop("value")
-    ).toBe(numberOfEvents);
-  });
-
-  test("change state when input changes", () => {
-    const eventObject = { target: { value: 32 } };
-    NumberOfEventsWrapper.find("#numberOfEvents__input").simulate(
-      "change",
-      eventObject
-    );
-    expect(NumberOfEventsWrapper.state("numberOfEvents")).toBe(32);
-  });
-
-  test("show number of events input label", () => {
-    expect(NumberOfEventsWrapper.find(".numberOfEvents label")).toHaveLength(1);
-  });
-});
+export default NumberOfEvents;
