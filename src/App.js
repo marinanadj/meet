2931d763b3389833eb951498691c01a4caae8bfa
Marina Nadj <marinanadj7@yahoo.com>
2022-06-22 19:32:11 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,165 +1,68 @@
-import './nprogress.css';
-import './App.css';
-import React, { Component } from 'react';
-import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
-import CitySearch from './CitySearch';
-import EventList from './EventList';
-import NumberOfEvents from './NumberOfEvents';
+import React, { Component } from "react";
+import { extractLocations, getEvents } from "./api";
+import "./App.css";
+import CitySearch from "./CitySearch";
+import EventList from "./EventList";
+import "./nprogress.css";
+import NumberOfEvents from "./NumberOfEvents";
 import { OfflineAlert } from './Alert';
-import WelcomeScreen from './WelcomeScreen';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import EventGenre from './EventGenre';
-import { Col, Container, Row, Navbar } from "react-bootstrap";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
+    currentLocation: "Everywhere",
     numberOfEvents: 32,
-    currentLocation: 'all',
-    showWelcomeScreen: undefined
-  }
+  };
 
-  async componentDidMount() {
+  componentDidMount() {
     this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get('code');
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
-        }
-      });
-    }
-
-    if (!navigator.onLine) {
-      this.setState({
-        offlineText:
-        'Your are currently offline. The displayed events might not be up to date.'
-      });
-    } else {
-      this.setState({
-        offlineText: ''
-      });
-    }
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
   }
 
   componentWillUnmount() {
     this.mounted = false;
   }
 
-  updateNumberOfEvents = (numberOfEvents) => {
-    this.setState(
-      {
-        numberOfEvents,
-      },
-      this.updateEvents(this.state.locations, numberOfEvents)
-    );
-  };
-
-  /* updateEvents = (location)  => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-      events : 
-      events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
-      });
-    });
-  }; */
-
-  updateEvents = (location, eventCount) => {
+  updateEvents = (
+    location = this.state.currentLocation,
+    eventCount = this.state.numberOfEvents
+  ) => {
     getEvents().then((events) => {
       const locationEvents =
-        location === "all"
+        location === "Everywhere"
           ? events
           : events.filter((event) => event.location === location);
-      if (this.mounted) {
-        this.setState({
-          events: locationEvents.slice(0, this.state.numberOfEvents),
-          currentLocation: location,
-        });
-      }
+
+      this.setState({
+        events: locationEvents.slice(0, eventCount),
+        currentLocation: location,
+      });
     });
   };
 
-  getData = () => {
-    const {locations, events} = this.state;
-    const data = locations.map((location) => {
-      const number = events.filter((event) => event.location === location).length;
-      const city = location.split(', ').shift();
-      return { city, number };
-    })
-    return data;
+  updateNumberOfEvents = (numberOfEvents) => {
+    this.setState({ numberOfEvents });
+    this.updateEvents(this.currentLocation, numberOfEvents);
   };
 
   render() {
-    if (this.state.showWelcomeScreen === undefined) return <div className='App' />
-    const { events, locations, numberOfEvents, offlineText } = this.state;
     return (
       <div className='App'>
-
-      <Navbar fixed='top' bg="light" expand="lg">
-        <Container className='navbar-brand-wrapper'>
-          <ul class='navbar-nav mx-auto'>
-            <li>
-              <a className='navbar-brand' href="#top">meet</a>
-            </li>
-          </ul>
-        </Container>
-    </Navbar>
-
-        <div className='search-wrapper'>
-        
-        <CitySearch locations={locations} updateEvents={this.updateEvents}/>
-
-        
-        <NumberOfEvents numberOfEvents={numberOfEvents} 
-            updateNumberOfEvents={this.updateNumberOfEvents}/>
-        </div>
-
-        <div className='data-vis'>
-        <h4 className='data-vis-head'>Events in each city</h4>
-        <div className='data-vis-wrapper'>
-        <EventGenre events={events} />
-        <ResponsiveContainer height={400}>
-        <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-            <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="City" />
-            <YAxis 
-              allowDecimals={false}
-              type="number" 
-              dataKey="number" 
-              name="Number of events" />
-            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-            <Scatter data={this.getData()} fill="#76c893" />
-        </ScatterChart>
-        </ResponsiveContainer>
-        </div>
-        </div>
-
-        <Container fluid>
-        <Row className='justify-content-md-center'>
-        <Col xs={12} md={6} xl={6}>
-        <EventList events={events}/>
-        </Col>
-        </Row>
-        </Container>
-
-        <OfflineAlert text={offlineText} />
-
-        <WelcomeScreen 
-          showWelcomeScreen={this.state.showWelcomeScreen} 
-          getAccessToken={()=> { getAccessToken() }} 
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
         />
-
+      <NumberOfEvents updateNumberOfEvents={this.updateNumberOfEvents} updateEvents={() => this.updateEvents()} />
+        <EventList events={this.state.events} />
+        {!navigator.onLine && <OfflineAlert text={'You are currently offline, data may be not updated.'}/>}
       </div>
     );
   }
 }
-
 
 export default App;
--- src/features/specifyNumberEvents.test.js
+++ src/features/specifyNumberEvents.test.js
@@ -1,44 +1,43 @@
-import { loadFeature, defineFeature } from 'jest-cucumber';
+import { loadFeature, defineFeature } from "jest-cucumber";
 import React from "react";
 import { mount } from "enzyme";
-import App from "../App";
 import NumberOfEvents from "../NumberOfEvents";
+import App from "../App";
 
-const feature = loadFeature('./src/features/specifyNumberEvents.feature');
-let AppWrapper;
+const feature = loadFeature ('./src/features/specifyNumberOfEvents.feature');
 
-defineFeature(feature, test => {
+defineFeature(feature, (test) => {
+    let AppWrapper;
 
-    
-
-    test('When user hasn’t specified a number, 12 is the default number', ({ given, when, then }) => {
-        given('the user has not specified a number of events to show', () => {
-            AppWrapper = mount(<App />);
+    test('When user hasn\'t specified a number, 32 is the default number.', ({ given, when, then }) => {
+        given('the user is on the main page of the app', async () => {
+            AppWrapper = await mount(<App />);
         });
 
-        when('the page loads', () => {
+        when('the user hasn\'t specified a number of events', () => {
             AppWrapper.update();
         });
 
-        then('12 events should be displayed (unless there are less available)', () => {
+        then('the default number of displayed events will be 32', () => {
             expect(AppWrapper.find('.event')).toHaveLength(2);
         });
     });
 
-    test('When the user types a number into the textbox, the number of events displayed should match the input number', ({ given, when, then }) => {
-        given('the main page is open', () => {
-            AppWrapper = mount(<App />);        
+    test('User can change the number of events they want to see.', ({ given, when, then }) => {
+        given('the user is on the main page', async () => {
+            AppWrapper = await mount(<App />);
         });
 
-        when('the user selects a different amount into the textbox', () => {
-            AppWrapper.update();
-            AppWrapper.find('.events_number__input').simulate('change', { target: { value: '1' } });
+        when('the user set a number of events he or she wants to see in the “Number of events” box', () => {
+            const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents)
+            NumberOfEventsWrapper.find('.events_number__input').simulate('change', { target: { value: 1 } });
         });
 
-        then('the number of events displayed should match the number', () => {
-            // const NumberOfEventsWrapper = mount(<NumberOfEvents />);
+        then('this number of events will be displayed', () => {
+            const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
             AppWrapper.update();
-            expect(AppWrapper.find(".event")).toHaveLength(1);
+
+            expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(1)
         });
     });
-});
+})
